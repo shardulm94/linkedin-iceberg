shirshanka/iceberg

Row level filtering: Allow table scans to pass a row level filter for ORC files

orc/src/main/java/org/apache/iceberg/orc/OrcIterable.java
@@ -19,8 +19,10 @@
 
 package org.apache.iceberg.orc;
 
+import com.google.common.collect.Sets;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.function.Function;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.Schema;
@@ -28,6 +30,7 @@ import org.apache.iceberg.exceptions.RuntimeIOException;
 import org.apache.iceberg.io.CloseableGroup;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.io.InputFile;
+import org.apache.iceberg.types.TypeUtil;
 import org.apache.orc.Reader;
 import org.apache.orc.TypeDescription;
 import org.apache.orc.storage.ql.exec.vector.VectorizedRowBatch;
@@ -42,16 +45,17 @@ class OrcIterable<T> extends CloseableGroup implements CloseableIterable<T> {
   private final Long start;
   private final Long length;
   private final Function<TypeDescription, OrcValueReader<?>> readerFunction;
+  private final OrcRowFilter rowFilter;
 
-  OrcIterable(InputFile file, Configuration config, Schema schema,
-              Long start, Long length,
-              Function<TypeDescription, OrcValueReader<?>> readerFunction) {
+  OrcIterable(InputFile file, Configuration config, Schema schema, Long start, Long length,
+      Function<TypeDescription, OrcValueReader<?>> readerFunction, OrcRowFilter rowFilter) {
     this.schema = schema;
     this.readerFunction = readerFunction;
     this.file = file;
     this.start = start;
     this.length = length;
     this.config = config;
+    this.rowFilter = rowFilter;
   }
 
   @SuppressWarnings("unchecked")
@@ -60,10 +64,25 @@ class OrcIterable<T> extends CloseableGroup implements CloseableIterable<T> {
     Reader orcFileReader = ORC.newFileReader(file, config);
     addCloseable(orcFileReader);
     TypeDescription readOrcSchema = ORCSchemaUtil.buildOrcProjection(schema, orcFileReader.getSchema());
+    if (rowFilter == null) {
+      return new OrcIterator(
+          newOrcIterator(file, readOrcSchema, start, length, orcFileReader),
+          readerFunction.apply(readOrcSchema), null, null);
+    } else {
+      Set<Integer> filterColumnIds = TypeUtil.getProjectedIds(rowFilter.requiredSchema());
+      Set<Integer> filterColumnIdsNotInReadSchema = Sets.difference(filterColumnIds, TypeUtil.getProjectedIds(schema));
+      Schema extraFilterColumns = TypeUtil.select(rowFilter.requiredSchema(), filterColumnIdsNotInReadSchema);
+      Schema finalReadSchema = TypeUtil.join(schema, extraFilterColumns);
 
-    return new OrcIterator(
-        newOrcIterator(file, readOrcSchema, start, length, orcFileReader),
-        readerFunction.apply(readOrcSchema));
+      TypeDescription finalReadOrcSchema = ORCSchemaUtil.buildOrcProjection(finalReadSchema, orcFileReader.getSchema());
+      TypeDescription rowFilterOrcSchema = ORCSchemaUtil.buildOrcProjection(rowFilter.requiredSchema(),
+          orcFileReader.getSchema());
+      RowFilterValueReader filterReader = new RowFilterValueReader(finalReadOrcSchema, rowFilterOrcSchema);
+
+      return new OrcIterator(
+          newOrcIterator(file, finalReadOrcSchema, start, length, orcFileReader),
+          readerFunction.apply(readOrcSchema), rowFilter, filterReader);
+    }
   }
 
   private static VectorizedRowBatchIterator newOrcIterator(InputFile file,
@@ -85,32 +104,62 @@ class OrcIterable<T> extends CloseableGroup implements CloseableIterable<T> {
 
   private static class OrcIterator<T> implements Iterator<T> {
 
-    private int nextRow;
-    private VectorizedRowBatch current;
+    private int currentRow;
+    private VectorizedRowBatch currentBatch;
+    private boolean advanced = false;
 
     private final VectorizedRowBatchIterator batchIter;
     private final OrcValueReader<T> reader;
+    private final OrcRowFilter filter;
+    private final RowFilterValueReader filterReader;
 
-    OrcIterator(VectorizedRowBatchIterator batchIter, OrcValueReader<T> reader) {
+    OrcIterator(VectorizedRowBatchIterator batchIter, OrcValueReader<T> reader, OrcRowFilter filter,
+        RowFilterValueReader filterReader) {
       this.batchIter = batchIter;
       this.reader = reader;
-      current = null;
-      nextRow = 0;
+      this.filter = filter;
+      this.filterReader = filterReader;
+      currentBatch = null;
+      currentRow = 0;
+    }
+
+    private void advance() {
+      if (!advanced) {
+        while (true) {
+          currentRow++;
+          // if batch has been consumed, move to next batch
+          if (currentBatch == null || currentRow >= currentBatch.size) {
+            if (batchIter.hasNext()) {
+              currentBatch = batchIter.next();
+              currentRow = 0;
+            } else {
+              // no more batches left to process
+              currentBatch = null;
+              currentRow = -1;
+              break;
+            }
+          }
+          if (filter == null || filter.shouldKeep(filterReader.read(currentBatch, currentRow))) {
+            // we have found our row
+            break;
+          }
+        }
+        advanced = true;
+      }
     }
 
     @Override
     public boolean hasNext() {
-      return (current != null && nextRow < current.size) || batchIter.hasNext();
+      advance();
+      return currentBatch != null;
     }
 
     @Override
     public T next() {
-      if (current == null || nextRow >= current.size) {
-        current = batchIter.next();
-        nextRow = 0;
-      }
-
-      return this.reader.read(current, nextRow++);
+      advance();
+      // mark current row as used
+      advanced = false;
+      return this.reader.read(currentBatch, currentRow);
     }
   }
 

orc/src/main/java/org/apache/iceberg/orc/RowFilterValueReader.java
@@ -1,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.orc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.orc.TypeDescription;
+import org.apache.orc.storage.ql.exec.vector.BytesColumnVector;
+import org.apache.orc.storage.ql.exec.vector.ColumnVector;
+import org.apache.orc.storage.ql.exec.vector.LongColumnVector;
+import org.apache.orc.storage.ql.exec.vector.VectorizedRowBatch;
+
+class RowFilterValueReader implements OrcValueReader<Object[]> {
+
+  private final Converter[] converters;
+  private final List<TypeDescription> columns;
+  private final int[] filterSchemaToReadSchemaColumnIndex;
+
+  RowFilterValueReader(TypeDescription readSchema, TypeDescription filterSchema) {
+    columns = filterSchema.getChildren();
+    converters = buildConverters();
+    filterSchemaToReadSchemaColumnIndex = buildFilterSchemaToReadSchemaColumnIndex(readSchema, filterSchema);
+  }
+
+  private int[] buildFilterSchemaToReadSchemaColumnIndex(TypeDescription readSchema, TypeDescription filterSchema) {
+    int[] index = new int[filterSchema.getChildren().size()];
+    List<String> filterFieldNames = filterSchema.getFieldNames();
+    List<String> readSchemaFieldNames = readSchema.getFieldNames();
+    Map<String, Integer> readSchemaFieldNameToIndex = new HashMap<>();
+    for (int i = 0; i < readSchemaFieldNames.size(); i++) {
+      readSchemaFieldNameToIndex.put(readSchemaFieldNames.get(i), i);
+    }
+    for (int i = 0; i < filterFieldNames.size(); i++) {
+      index[i] = readSchemaFieldNameToIndex.get(filterFieldNames.get(i));
+    }
+    return index;
+  }
+
+  @Override
+  public Object[] read(VectorizedRowBatch batch, int row) {
+    Object[] rowFields = new Object[converters.length];
+    for (int c = 0; c < converters.length; ++c) {
+      rowFields[c] = converters[c].convert(batch.cols[filterSchemaToReadSchemaColumnIndex[c]], row);
+    }
+    return rowFields;
+  }
+
+  interface Converter<T> {
+    default T convert(ColumnVector vector, int row) {
+      int rowIndex = vector.isRepeating ? 0 : row;
+      if (!vector.noNulls && vector.isNull[rowIndex]) {
+        return null;
+      } else {
+        return convertNonNullValue(vector, rowIndex);
+      }
+    }
+
+    T convertNonNullValue(ColumnVector vector, int row);
+  }
+
+  private Converter[] buildConverters() {
+    Converter[] newConverters = new Converter[columns.size()];
+    for (int c = 0; c < newConverters.length; ++c) {
+      newConverters[c] = buildConverter(columns.get(c));
+    }
+    return newConverters;
+  }
+
+  private static Converter buildConverter(final TypeDescription schema) {
+    switch (schema.getCategory()) {
+      case INT:
+        return new IntConverter();
+      case LONG:
+        String longAttributeValue = schema.getAttributeValue(ORCSchemaUtil.ICEBERG_LONG_TYPE_ATTRIBUTE);
+        ORCSchemaUtil.LongType longType = longAttributeValue == null ? ORCSchemaUtil.LongType.LONG :
+            ORCSchemaUtil.LongType.valueOf(longAttributeValue);
+        switch (longType) {
+          case LONG:
+            return new LongConverter();
+          default:
+            throw new IllegalStateException("Unhandled Long type found in ORC type attribute: " + longType);
+        }
+      case STRING:
+      case CHAR:
+      case VARCHAR:
+        return new StringConverter();
+      default:
+        throw new IllegalArgumentException("Unhandled type " + schema);
+    }
+  }
+
+  private static class IntConverter implements Converter<Integer> {
+    @Override
+    public Integer convertNonNullValue(ColumnVector vector, int row) {
+      return (int) ((LongColumnVector) vector).vector[row];
+    }
+  }
+
+  private static class LongConverter implements Converter<Long> {
+    @Override
+    public Long convertNonNullValue(ColumnVector vector, int row) {
+      return ((LongColumnVector) vector).vector[row];
+    }
+  }
+
+  private static class StringConverter implements Converter<String> {
+    @Override
+    public String convertNonNullValue(ColumnVector vector, int row) {
+      BytesColumnVector bytesVector = (BytesColumnVector) vector;
+      return new String(bytesVector.vector[row], bytesVector.start[row], bytesVector.length[row],
+          StandardCharsets.UTF_8);
+    }
+  }
+}

shardulm94/linkedin-iceberg

orc/src/main/java/org/apache/iceberg/orc/OrcIterable.java
@@ -20,6 +20,7 @@
 package org.apache.iceberg.orc;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.function.Function;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.Schema;
@@ -32,6 +33,9 @@ import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.io.CloseableIterator;
 import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.util.Pair;
+import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
+import org.apache.iceberg.relocated.com.google.common.collect.Sets;
+import org.apache.iceberg.types.TypeUtil;
 import org.apache.orc.Reader;
 import org.apache.orc.TypeDescription;
 import org.apache.orc.storage.ql.exec.vector.VectorizedRowBatch;
@@ -51,11 +55,13 @@ class OrcIterable<T> extends CloseableGroup implements CloseableIterable<T> {
   private final boolean caseSensitive;
   private final Function<TypeDescription, OrcBatchReader<?>> batchReaderFunction;
   private final int recordsPerBatch;
+  private final OrcRowFilter rowFilter;
 
   OrcIterable(InputFile file, Configuration config, Schema schema,
               Long start, Long length,
               Function<TypeDescription, OrcRowReader<?>> readerFunction, boolean caseSensitive, Expression filter,
-              Function<TypeDescription, OrcBatchReader<?>> batchReaderFunction, int recordsPerBatch) {
+              Function<TypeDescription, OrcBatchReader<?>> batchReaderFunction, int recordsPerBatch,
+              OrcRowFilter rowFilter) {
     this.schema = schema;
     this.readerFunction = readerFunction;
     this.file = file;
@@ -66,6 +72,7 @@ class OrcIterable<T> extends CloseableGroup implements CloseableIterable<T> {
     this.filter = (filter == Expressions.alwaysTrue()) ? null : filter;
     this.batchReaderFunction = batchReaderFunction;
     this.recordsPerBatch = recordsPerBatch;
+    this.rowFilter = rowFilter;
   }
 
   @SuppressWarnings("unchecked")
@@ -81,16 +88,36 @@ class OrcIterable<T> extends CloseableGroup implements CloseableIterable<T> {
       sarg = ExpressionToSearchArgument.convert(boundFilter, readOrcSchema);
     }
 
-    VectorizedRowBatchIterator rowBatchIterator = newOrcIterator(file, readOrcSchema, start, length, orcFileReader,
-        sarg, recordsPerBatch);
-    if (batchReaderFunction != null) {
-      OrcBatchReader<T> batchReader = (OrcBatchReader<T>) batchReaderFunction.apply(readOrcSchema);
-      return CloseableIterator.transform(rowBatchIterator, pair -> {
-        batchReader.setBatchContext(pair.second());
-        return batchReader.read(pair.first());
-      });
+    if (rowFilter == null) {
+      VectorizedRowBatchIterator rowBatchIterator = newOrcIterator(file, readOrcSchema, start, length, orcFileReader,
+          sarg, recordsPerBatch);
+      if (batchReaderFunction != null) {
+        OrcBatchReader<T> batchReader = (OrcBatchReader<T>) batchReaderFunction.apply(readOrcSchema);
+        return CloseableIterator.transform(rowBatchIterator, pair -> {
+          batchReader.setBatchContext(pair.second());
+          return batchReader.read(pair.first());
+        });
+      } else {
+        return new OrcRowIterator<>(rowBatchIterator, (OrcRowReader<T>) readerFunction.apply(readOrcSchema));
+      }
     } else {
-      return new OrcRowIterator<>(rowBatchIterator, (OrcRowReader<T>) readerFunction.apply(readOrcSchema));
+      Preconditions.checkArgument(batchReaderFunction == null,
+          "Row-level filtering not supported by vectorized reader");
+      Set<Integer> filterColumnIds = TypeUtil.getProjectedIds(rowFilter.requiredSchema());
+      Set<Integer> filterColumnIdsNotInReadSchema = Sets.difference(filterColumnIds,
+          TypeUtil.getProjectedIds(schema));
+      Schema extraFilterColumns = TypeUtil.select(rowFilter.requiredSchema(), filterColumnIdsNotInReadSchema);
+      Schema finalReadSchema = TypeUtil.join(schema, extraFilterColumns);
+
+      TypeDescription finalReadOrcSchema = ORCSchemaUtil.buildOrcProjection(finalReadSchema,
+          orcFileReader.getSchema());
+      TypeDescription rowFilterOrcSchema = ORCSchemaUtil.buildOrcProjection(rowFilter.requiredSchema(),
+          orcFileReader.getSchema());
+      RowFilterValueReader filterReader = new RowFilterValueReader(finalReadOrcSchema, rowFilterOrcSchema);
+
+      return new OrcRowIterator<>(
+          newOrcIterator(file, finalReadOrcSchema, start, length, orcFileReader, sarg, recordsPerBatch),
+          (OrcRowReader<T>) readerFunction.apply(readOrcSchema), rowFilter, filterReader);
     }
   }
 
@@ -116,34 +143,67 @@ class OrcIterable<T> extends CloseableGroup implements CloseableIterable<T> {
 
   private static class OrcRowIterator<T> implements CloseableIterator<T> {
 
-    private int nextRow;
-    private VectorizedRowBatch current;
+    private int currentRow;
+    private VectorizedRowBatch currentBatch;
+    private boolean advanced = false;
 
     private final VectorizedRowBatchIterator batchIter;
     private final OrcRowReader<T> reader;
+    private final OrcRowFilter filter;
+    private final RowFilterValueReader filterReader;
 
-    OrcRowIterator(VectorizedRowBatchIterator batchIter, OrcRowReader<T> reader) {
+    OrcRowIterator(VectorizedRowBatchIterator batchIter, OrcRowReader<T> reader, OrcRowFilter filter,
+        RowFilterValueReader filterReader) {
       this.batchIter = batchIter;
       this.reader = reader;
-      current = null;
-      nextRow = 0;
+      this.filter = filter;
+      this.filterReader = filterReader;
+      currentBatch = null;
+      currentRow = 0;
+    }
+
+    private void advance() {
+      if (!advanced) {
+        while (true) {
+          currentRow++;
+          // if batch has been consumed, move to next batch
+          if (currentBatch == null || currentRow >= currentBatch.size) {
+            if (batchIter.hasNext()) {
+              Pair<VectorizedRowBatch, Long> nextBatch = batchIter.next();
+              currentBatch = nextBatch.first();
+              currentRow = 0;
+              reader.setBatchContext(nextBatch.second());
+              if (filterReader != null) {
+                filterReader.setBatchContext(nextBatch.second());
+              }
+            } else {
+              // no more batches left to process
+              currentBatch = null;
+              currentRow = -1;
+              break;
+            }
+          }
+          if (filter == null || filter.shouldKeep(filterReader.read(currentBatch, currentRow))) {
+            // we have found our row
+            break;
+          }
+        }
+        advanced = true;
+      }
     }
 
     @Override
     public boolean hasNext() {
-      return (current != null && nextRow < current.size) || batchIter.hasNext();
+      advance();
+      return currentBatch != null;
     }
 
     @Override
     public T next() {
-      if (current == null || nextRow >= current.size) {
-        Pair<VectorizedRowBatch, Long> nextBatch = batchIter.next();
-        current = nextBatch.first();
-        nextRow = 0;
-        this.reader.setBatchContext(nextBatch.second());
-      }
-
-      return this.reader.read(current, nextRow++);
+      advance();
+      // mark current row as used
+      advanced = false;
+      return this.reader.read(currentBatch, currentRow);
     }
 
     @Override

orc/src/main/java/org/apache/iceberg/orc/RowFilterValueReader.java
@@ -1,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.orc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.orc.TypeDescription;
+import org.apache.orc.storage.ql.exec.vector.BytesColumnVector;
+import org.apache.orc.storage.ql.exec.vector.ColumnVector;
+import org.apache.orc.storage.ql.exec.vector.LongColumnVector;
+import org.apache.orc.storage.ql.exec.vector.VectorizedRowBatch;
+
+class RowFilterValueReader implements OrcRowReader<Object[]> {
+
+  private final Converter[] converters;
+  private final List<TypeDescription> columns;
+  private final int[] filterSchemaToReadSchemaColumnIndex;
+
+  RowFilterValueReader(TypeDescription readSchema, TypeDescription filterSchema) {
+    columns = filterSchema.getChildren();
+    converters = buildConverters();
+    filterSchemaToReadSchemaColumnIndex = buildFilterSchemaToReadSchemaColumnIndex(readSchema, filterSchema);
+  }
+
+  private int[] buildFilterSchemaToReadSchemaColumnIndex(TypeDescription readSchema, TypeDescription filterSchema) {
+    int[] index = new int[filterSchema.getChildren().size()];
+    List<String> filterFieldNames = filterSchema.getFieldNames();
+    List<String> readSchemaFieldNames = readSchema.getFieldNames();
+    Map<String, Integer> readSchemaFieldNameToIndex = new HashMap<>();
+    for (int i = 0; i < readSchemaFieldNames.size(); i++) {
+      readSchemaFieldNameToIndex.put(readSchemaFieldNames.get(i), i);
+    }
+    for (int i = 0; i < filterFieldNames.size(); i++) {
+      index[i] = readSchemaFieldNameToIndex.get(filterFieldNames.get(i));
+    }
+    return index;
+  }
+
+  @Override
+  public Object[] read(VectorizedRowBatch batch, int row) {
+    Object[] rowFields = new Object[converters.length];
+    for (int c = 0; c < converters.length; ++c) {
+      rowFields[c] = converters[c].convert(batch.cols[filterSchemaToReadSchemaColumnIndex[c]], row);
+    }
+    return rowFields;
+  }
+
+  interface Converter<T> {
+    default T convert(ColumnVector vector, int row) {
+      int rowIndex = vector.isRepeating ? 0 : row;
+      if (!vector.noNulls && vector.isNull[rowIndex]) {
+        return null;
+      } else {
+        return convertNonNullValue(vector, rowIndex);
+      }
+    }
+
+    T convertNonNullValue(ColumnVector vector, int row);
+  }
+
+  private Converter[] buildConverters() {
+    Converter[] newConverters = new Converter[columns.size()];
+    for (int c = 0; c < newConverters.length; ++c) {
+      newConverters[c] = buildConverter(columns.get(c));
+    }
+    return newConverters;
+  }
+
+  private static Converter buildConverter(final TypeDescription schema) {
+    switch (schema.getCategory()) {
+      case INT:
+        return new IntConverter();
+      case LONG:
+        String longAttributeValue = schema.getAttributeValue(ORCSchemaUtil.ICEBERG_LONG_TYPE_ATTRIBUTE);
+        ORCSchemaUtil.LongType longType = longAttributeValue == null ? ORCSchemaUtil.LongType.LONG :
+            ORCSchemaUtil.LongType.valueOf(longAttributeValue);
+        switch (longType) {
+          case LONG:
+            return new LongConverter();
+          default:
+            throw new IllegalStateException("Unhandled Long type found in ORC type attribute: " + longType);
+        }
+      case STRING:
+      case CHAR:
+      case VARCHAR:
+        return new StringConverter();
+      default:
+        throw new IllegalArgumentException("Unhandled type " + schema);
+    }
+  }
+
+  private static class IntConverter implements Converter<Integer> {
+    @Override
+    public Integer convertNonNullValue(ColumnVector vector, int row) {
+      return (int) ((LongColumnVector) vector).vector[row];
+    }
+  }
+
+  private static class LongConverter implements Converter<Long> {
+    @Override
+    public Long convertNonNullValue(ColumnVector vector, int row) {
+      return ((LongColumnVector) vector).vector[row];
+    }
+  }
+
+  private static class StringConverter implements Converter<String> {
+    @Override
+    public String convertNonNullValue(ColumnVector vector, int row) {
+      BytesColumnVector bytesVector = (BytesColumnVector) vector;
+      return new String(bytesVector.vector[row], bytesVector.start[row], bytesVector.length[row],
+          StandardCharsets.UTF_8);
+    }
+  }
+}